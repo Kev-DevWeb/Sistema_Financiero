bae96171a7b6534942ffbae5131ca4e82d9aa1b0
KevGRDev <90330077+KevGRDev@users.noreply.github.com>
2022-12-07 19:17:01 -0600

    Intento de conexion bd

---
+++ Sistema_Financiero/conectarbd.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Sistema_Financiero
+{
+    internal class conectarbd
+    {
+        string cadena = "Data Source = DESKTOP-IL89A7G\\SQLEXPRESS;Initial Catalog = sistemaFinanciero; Integrated Security = True ";
+        public SqlConnection conectarbbd = new SqlConnection();
+
+        public conectarbd()
+        {
+            
+             conectarbbd.ConnectionString = cadena;           
+
+        }
+
+
+        public void abrir ()
+        {
+            try
+            {
+                conectarbbd.Open ();
+                Console.WriteLine("Conexion Abierta! :D");
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al conectar D: : " + ex.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+
+
+
+
+        public void cerrar ()
+        {
+            conectarbbd.Close ();
+
+            Console.ReadLine();
+
+        }
+
+
+
+       
+    }
+}
---
+++ Sistema_Financiero/conectarsql.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Sistema_Financiero
+{
+    public partial class conectarsql : Form
+    {
+        public conectarsql()
+        {
+            InitializeComponent();
+        }
+
+        private void conectarsql_Load(object sender, EventArgs e)
+        {
+            conectarbd conexion = new conectarbd();
+            conexion.abrir();
+            Console.ReadLine();
+        }
+    }
+
+   
+}
